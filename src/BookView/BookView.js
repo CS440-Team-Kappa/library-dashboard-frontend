4b62f3c91be04a065ddbea2de179a91d9a761fa2
skylerputney <121919826+skylerputney@users.noreply.github.com>
2024-04-18 19:24:07 -0400

    BookView - Search bar
    - Added frontend needed for search bar
    - Duplicate entries will currently show until a libraryID is added from a global
          - could create a different view within backend that just doesn't SELECT bc.LibraryID or group by it and use an endpoint to this view when not filtering by specific library (user not logged in or all libraries filtered) if we want

--- src/BookView/BookView.js
+++ src/BookView/BookView.js
@@ -5,12 +5,15 @@ import BookList from './../Views/BookList';
 
 function BookView() {
     const [books, setBooks] = useState([]);
+    const [searchString, setSearchString] = useState('');
 
     useEffect(() => {
         //Fetch data from backend
         const fetchData = async () => {
             try {
-                const response = await axios.get('http://127.0.0.1:8000/booklists/');
+                //if searchString present, set is as 'searchString' in params for backend purposes
+                const params = searchString ? { searchString: searchString } : {};
+                const response = await axios.get('http://127.0.0.1:8000/booklists/', {params}); //Need to change to booklists/${libraryID}/ once info available globally
                 setBooks(response.data);
             } catch (e) {
                 console.error('Error fetching data: ', e);
@@ -18,12 +21,12 @@ function BookView() {
         };
 
         fetchData();
-    }, []);
+    }, [searchString]);
   return (
     <div className="BookView">
         <div className='searchbar'>
-            <form className='searchform'>
-                <input className='inputbar' type='text'></input>
+              <form className='searchform' onSubmit={(e) => e.preventDefault()}>
+                  <input className='inputbar' type='text' value={searchString} onChange={(e) => setSearchString(e.target.value)} placeholder="Search for a book by author or title" />
                 <button className='barbutton' type='submit'>Search</button>
             </form>
             <hr />
