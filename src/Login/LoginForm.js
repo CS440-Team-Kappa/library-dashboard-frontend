363ab771595d0a5ee48156d41223158f09251323
Michael Dolan <md0174@mix.wvu.edu>
2024-04-24 17:11:39 -0400

    Nests login body

--- src/Login/LoginForm.js
+++ src/Login/LoginForm.js
@@ -4,17 +4,19 @@ import './LoginPage.css';
 function LoginForm() {
     return (
         <div className="login-default">
-            <div className="login-containment">
-                <h1>Login</h1>
-                <div className="login-input">
-                    <input type="text" placeholder="Username" required />
-                </div>
-                <div className="login-input">
-                    <input type="password" placeholder="Password" required />
-                </div>
-                <button type="submit" className="submit-btn">Login</button>
-                <div className="create-account-link">
-                    <p>Create an account here: <a href="#">Signup</a></p>
+            <div className="login-container-body">
+                <div className="login-containment">
+                    <h1>Login</h1>
+                    <div className="login-input">
+                        <input type="text" placeholder="Username" required />
+                    </div>
+                    <div className="login-input">
+                        <input type="password" placeholder="Password" required />
+                    </div>
+                    <button type="submit" className="submit-btn">Login</button>
+                    <div className="create-account-link">
+                        <p>Create an account here: <a href="#">Signup</a></p>
+                    </div>
                 </div>
             </div>
         </div>
