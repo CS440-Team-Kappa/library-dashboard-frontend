dc8e5be529b465f1f4307f443fb77461ddb57277
Michael Dolan <md0174@mix.wvu.edu>
2024-04-24 15:51:02 -0400

    Modified App.js to implement new LoginPage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './Dashboard/Dashboard';
-import Login from './Login/Login';
+import Login from './Login/LoginPage';
 import CreateAccount from './CreateAccount/CreateAccount';
 
 function App() {
