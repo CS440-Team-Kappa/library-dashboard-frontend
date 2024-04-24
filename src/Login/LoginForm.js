3e07b41651693dfd21b9f285dc5590087738785b
Michael Dolan <md0174@mix.wvu.edu>
2024-04-24 16:44:52 -0400

    Instead of nesting put default in body

--- src/Login/LoginForm.js
+++ src/Login/LoginForm.js
@@ -3,19 +3,17 @@ import './LoginPage.css';
 
 function LoginForm() {
     return (
-        <div className="login-default">
-            <div className="login-containment">
-                <h1>Login</h1>
-                <div className="login-input">
-                    <input type="text" placeholder="Username" required />
-                </div>
-                <div className="login-input">
-                    <input type="password" placeholder="Password" required />
-                </div>
-                <button type="submit" className="submit-btn">Login</button>
-                <div className="create-account-link">
-                    <p>Create an account here: <a href="#">Signup</a></p>
-                </div>
+        <div className="login-containment">
+            <h1>Login</h1>
+            <div className="login-input">
+                <input type="text" placeholder="Username" required />
+            </div>
+            <div className="login-input">
+                <input type="password" placeholder="Password" required />
+            </div>
+            <button type="submit" className="submit-btn">Login</button>
+            <div className="create-account-link">
+                <p>Create an account here: <a href="#">Signup</a></p>
             </div>
         </div>
     );
