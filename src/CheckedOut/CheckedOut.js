330c071729651ca893eefb53e1fc9e4404b05ab0
Michael Dolan <md0174@mix.wvu.edu>
2024-04-25 12:06:46 -0400

    Made table for CheckedOut, not reactive, can't figure out header

--- src/CheckedOut/CheckedOut.js
+++ src/CheckedOut/CheckedOut.js
@@ -2,8 +2,33 @@ import "./CheckedOut.css"
 
 function CheckedOut() {
     return (
-        <div className="CheckedOut">
-            <h1>List of User's checked out books</h1>
+        <div className="table-section">
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Copies</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Harry Potter</th>
+                        <td>JK Rowling</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Lord of the Rings</th>
+                        <td>JR Tolkien</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Calculus 1</th>
+                        <td>Jimmy Buffet</td>
+                        <td>10</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
