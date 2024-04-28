0f0cce63658c534f71106eb4971e2e9b49c0bec2
Michael Dolan <md0174@mix.wvu.edu>
2024-04-27 23:04:17 -0400

    Fixed label wrapping

--- src/CreateAccount/CreateAccount.js
+++ src/CreateAccount/CreateAccount.js
@@ -27,31 +27,41 @@ function CreateAccount() {
         <h1>Create Account</h1>
         <form onSubmit={handleSubmit}>
           <div className="create-input">
-            <label htmlFor="Email">Email:</label>
+            <div className="label-text">
+              <label htmlFor="Email">Email:</label>
+            </div>
             <div className="text-input">
               <input type="text" id="Email" ref={userRef} onChange={(e) => setEmail(e.target.value)} value={email} required />
             </div>
           </div>
           <div className="create-input">
-            <label htmlFor="password">Password</label>
+            <div className="label-text">
+              <label htmlFor="password">Password</label>
+            </div>
             <div className="text-input">
               <input type="password" id="password" onChange={(e) => setPwd(e.target.value)} value={pwd} required />
             </div>
           </div>
           <div className="create-input">
-            <label htmlFor="firstname">First Name</label>
+            <div className="label-text">
+              <label htmlFor="firstname">First Name</label>
+            </div>
             <div className="text-input">
               <input type="text" id="firstname" onChange={(e) => setFirstName(e.target.value)} value={firstName} required />
             </div>
           </div>
           <div className="create-input">
-            <label htmlFor="lastname">Last Name</label>
+            <div className="label-text">
+              <label htmlFor="lastname">Last Name</label>
+            </div>
             <div className="text-input">
               <input type="text" id="lastname" onChange={(e) => setLastName(e.target.value)} value={lastName} required />
             </div>
           </div>
           <div className="create-input">
-            <label htmlFor="phonenumber">Phone Number</label>
+            <div className="label-text">
+              <label htmlFor="phonenumber">Phone Number</label>
+            </div>
             <div className="text-input">
               <input type="text" id="phonenumber" onChange={(e) => setPhoneNumber(e.target.value)} value={phoneNumber} required />
             </div>
