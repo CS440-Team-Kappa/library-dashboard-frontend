a6d74b3e35765c106113b189174b19b23cf693fa
kenny-clark <krc00002@mix.wvu.edu>
2024-05-01 18:49:42 -0400

    Hide create adn login buttons

--- src/Actions/Actions.js
+++ src/Actions/Actions.js
@@ -1,16 +1,44 @@
 import "./Actions.css"
 import { useNavigate } from 'react-router-dom';
-import { handleClick } from 'react'
+import { handleClick, useState } from 'react'
+import Modal from '../Views/Modal';
+import UserProfile from './../Components/UserProfile';
+
 function Actions() {
     let Navigate = useNavigate();
     handleClick = () => Navigate('/createemployee');
 
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState('createEmployee');
+    const condition = UserProfile.isEmployee();
+    const condition2 = !(UserProfile.isLoggedIn());
+  
+    const handleOptionClick = (option) => {
+      setSelectedOption(option);
+      setModalOpen(true);
+    }
+  
+    const handleModalClose = () => {
+      setModalOpen(false);
+      setSelectedOption('addBook');
+    }
+
     return (
         <div className="Actions">
-            <a className='createAccount' href='/createaccount'>Create An Account</a>
+            {condition2 ? (
+                    <div>
+            <a className='createAccount' href='/createaccount'>Create An Account</a>   
             <a className="login" href='/login'>Login</a>
-            <button enabled="true" onClick={handleClick}>Create Employee</button>
+            </div>
+                ) : null}
+                {condition ? (
+                    <div>
+                        <button enabled="true" onClick={handleClick}>Create Employee</button>
+                    </div>
+                ) : null}
+
         </div>
+
     )
 }
 
